94d5c09847bf688850a0a15f9b998afaa03d8f19
Andre <andrecolon@gmail.com>
2020-05-12 18:52:14 -0700

    Demo my crazy folder

--- Desktop/School/gh-cc-gh/src/App.js
+++ Desktop/School/gh-cc-gh/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export class App extends React.Component {
+  //React.Component can only set state
   state = {
-    usersGH: []
+    usersGH: [],
+    error: "",
+    userName: "andrecolon"
   }
   componentDidMount() {
+    //
     fetch('https://api.github.com/users/andrecolon/followers')
       .then((response) => response.json())
       .then(userList => {
@@ -17,13 +21,48 @@ export class App extends React.Component {
     // make fetch request
   }
 
+handleUserChange = e => {
+    this.setState({
+      userName: e.target.value
+    });
+  };
+
+handleUserUpdate = e => {
+  fetch(`https://api.github.com/users/${this.state.userName}/followers`)
+  .then(res => {
+    console.log(res)
+    return res.json()
+  })
+  .then(user =>{
+    if(user.status === 'error'){
+      this.setState({error: user.error});
+      
+    } else {
+      this.setState({usersGH: user.login})
+    }
+  })
+  .catch(err =>{
+    console.log(err)
+    this.setState({error: err})
+  })
+}
   render() {
     return (
+      <div className="user-container">
+        <input
+          id="name"
+          text="text"
+          placeholder="User Name"
+          value={this.state.userName}
+          onChange={this.handleUserChange}
+        />
+        <button onClick={this.handleUserUpdate}>Next GH User</button>
       <ul className="user-grid">
-        {this.state.usersGH.map((user) => (
-          <li key={user.id}>{user.login}<img src={user.avatar_url} alt="User" /></li>
+        {this.state.usersGH.map((users) => (
+          <li key={users.id}>{users.login}<img src={users.avatar_url} alt="User" /></li>
         ))}
       </ul>
+      </div>
     )
   }
 }
